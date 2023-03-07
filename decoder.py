df52192bac7fc1abd39ac744722ea94ecce61319
DannyBeeman <dannybeeman@outlook.com>
2023-03-07 09:52:24 -0500

    added decoded and finished menu

--- decoder.py
+++ decoder.py
@@ -1,9 +1,15 @@
 def encode(password):
-    pass
-
-
-def decode(password):
-    pass
+    encoded_password = ""
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10)
+        encoded_password += encoded_digit
+    return encoded_password
+def decode(encoded_password): #daniel beeman
+    password = ""
+    for digit in encoded_password:
+        decoded_digit = str((int(digit) - 3) % 10)
+        password += decoded_digit
+    return password
 
 
 def main():
@@ -12,10 +18,18 @@ def main():
         print("Menu")
         print("-------------")
         print("1. Encode\n2. Decode\n3. Quit")
-
         option = input("Please enter an option: ")
 
         if option == "1":
+            password = str(input())
+            encoded_password = encode(password)
+            print(encoded_password)
+        if option == "2":
+            encoded = str(input())
+            decoded_password = decode(encoded)
+            print(decoded_password)
+        if option == "3":
+            exit()
 
 
 
