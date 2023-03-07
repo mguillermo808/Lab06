f9e5f021b091585f91167596f894827d8b78ff14
mguillermo808 <113198717+mguillermo808@users.noreply.github.com>
2023-03-07 09:43:12 -0500

    Add files via upload
    my file

---
+++ decoder.py
@@ -0,0 +1,23 @@
+def encode(password):
+    pass
+
+
+def decode(password):
+    pass
+
+
+def main():
+    option = True
+    while option:
+        print("Menu")
+        print("-------------")
+        print("1. Encode\n2. Decode\n3. Quit")
+
+        option = input("Please enter an option: ")
+
+        if option == "1":
+
+
+
+if __name__ == "__main__":
+    main()
